79d160348c3a8987eae3ed7f412746a2239c8666
Lizzie Coady <lizziecoady@hotmail.co.uk>
2019-03-06 11:53:16 +0000

    Added min temp

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -12,4 +12,21 @@ describe('Thermostat', function(){
     expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
 
+  it('increases the temperature with up(degrees)', function(){
+    thermostat.up()
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+
+  it('decreases the temperature with down(degrees)', function(){
+    thermostat.down()
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+
+  it('has a minimum of 10 degress', function(){
+    for (var i = 0; i < 11; i++) {
+      thermostat.down()
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
+  });
+
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function Thermostat(){
+  this.MININUM_TEMPERATURE = 10;
   this.temperature = 20;
 };
 
@@ -9,3 +10,18 @@ function Thermostat(){
 Thermostat.prototype.getCurrentTemperature = function(){
   return this.temperature;
 };
+
+Thermostat.prototype.up = function(){
+  return this.temperature += 1;
+};
+
+Thermostat.prototype.down = function(){
+  if (this.isMinimumTemperature()) {
+    return;
+  }
+  this.temperature -= 1;
+};
+
+Thermostat.prototype.isMinimumTemperature = function(){
+  return this.temperature === this.MININUM_TEMPERATURE;
+}
